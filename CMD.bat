973870831d54a8d69a0b0e5e81e7c4504623485b
Yonatan Israeli <yonirisraeli@gmail.com>
2024-01-22 23:07:29 -0500

    Add files via upload

---
+++ CMD.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+echo READ DISCLAIMER ^-^-^> THIS IS FOR EDUCATIONAL PURPOSES ONLY! DO NOT USE THIS ON SOMEONE ELSES PC WITHOUT THEIR EXPLICIT PERMISSION! THIS IS A HACK! CONTINUE AT YOUR OWN RISK! WE HOLD NO RESPONSIBILITY FOR ANYTHING THAT HAPPEND, IS HAPPENING, AND/OR WILL HAPPEN BECAUSE OF THIS BATCH FILE!
+goto :Disclaimer
+
+:Disclaimer
+echo.
+set /p Disclaimer="Do you agree to the Disclaimer? (Yes/No) "
+if /i "%Disclaimer%"=="Yes" goto :Start
+if /i "%Disclaimer%"=="No" goto :Exit
+echo Invalid Syntax!
+goto :Disclaimer
+
+:Start
+cls
+goto :CMD
+
+:CMD
+set Prompt="%cd%>"
+set /p Command=%Prompt%
+if /i "%Command%"=="exit" goto :Exit
+%Command%
+goto :CMD
+
+:Exit
+endlocal
+exit
