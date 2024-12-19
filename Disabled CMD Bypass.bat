ee4526d28dc4d00620aab20dde70301bd00a3587
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:48:07 -0500

    Update Disabled CMD Bypass.bat

--- Disabled CMD Bypass.bat
+++ Disabled CMD Bypass.bat
@@ -2,10 +2,11 @@
 setlocal
 title Disabled CMD Bypass
 echo Program Name: Disabled CMD Bypass
-echo Version: 2.1.6
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 2.1.7
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 goto "Disclaimer"
 
 :"Disclaimer"
